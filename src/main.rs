353b53bf71a77ebf7608a684b6b9ae7492c7176f
LBlend <lblend@protonmail.com>
2021-12-03 16:31:10 +0100

    add bluetooth name constant

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ use futures::stream::{StreamExt};
 use std::collections::HashMap;
 
 
+const THRESHOLD: i16 = -75;  // Blutetooth strength in dBm
+const BLUETOOTH_ADAPTER_NAME: &str = "hci0";
+
+
 pub async fn yeet() {
-     let session = Session::new().await.expect("I hate error handling");
-    let adapter = session.adapter("hci0").expect("I hate error handling");
+    let session = Session::new().await.expect("I hate error handling");
+    let adapter = session.adapter(BLUETOOTH_ADAPTER_NAME).expect("I hate error handling");
 
     let mut devices = HashMap::new();
 
-    let mut bluetooth_stream = adapter.discover_devices().await.expect("I really hate error handling");
+    let mut bluetooth_stream = adapter.discover_devices().await.expect("a I really hate error handling");
     while let Some(item) = bluetooth_stream.next().await {
         match item {
             AdapterEvent::DeviceAdded(mac_address) => {
-                let device = adapter.device(mac_address).expect("I really hate error handling");
+                let device = adapter.device(mac_address).expect("b I really hate error handling");
                 let name = device.alias().await.expect("A");
                 let rssi = device.rssi().await.expect("yeet");
                 match rssi {
                     Some(signalstrength) => {
-                        if signalstrength > -75 {
+                        if signalstrength > THRESHOLD {
                             devices.insert(mac_address, signalstrength);
                             println!("MAC: {}\tName: {}\tStrength: {}", mac_address, name, signalstrength);
                             println!("{:?} Devices connected", devices.len());
@@ -42,7 +46,6 @@ pub async fn yeet() {
         }
     }
 }
-
 
 #[tokio::main]
 pub async fn main() {
