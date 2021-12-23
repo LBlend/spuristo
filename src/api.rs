417bb49af49c1a4497425b7828560e0e308b69b7
LBlend <lblend@protonmail.com>
2021-12-23 23:38:08 +0100

    initialize interface

---
+++ src/api.rs
@@ -0,0 +1,11 @@
+pub fn get_training_data() {
+	todo!();
+}
+
+pub async fn insert_training_datapoint() {
+	todo!();
+}
+
+pub async fn insert_datapoint() {
+	todo!()
+}
---
+++ src/predict.rs
@@ -0,0 +1,3 @@
+fn predict() {
+	todo!();
+}
