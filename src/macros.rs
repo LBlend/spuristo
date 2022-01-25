c9ab77a16ceb2e7c06f63e6be6552bb3e3016b6b
LBlend <lblend@protonmail.com>
2022-01-25 14:41:11 +0100

    fix indentation

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! or_continue {
-	($e:expr) => {
-		match $e {
-			Ok(v) => v,
-			Err(_) => continue
-		}
-	};
+    ($e:expr) => {
+        match $e {
+            Ok(v) => v,
+            Err(_) => continue,
+        }
+    };
 }
